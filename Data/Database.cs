97b482b69c0624addd0c849247b0d87e96430cee
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-01-17 20:06:34 +0100

    Add Boardgame model and database

---
+++ Data/Database.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiBoardgameCafeApp.Models;
+using SQLite;
+
+namespace MauiBoardgameCafeApp.Data
+{
+	internal class Database
+	{
+		private readonly SQLiteAsyncConnection _connection;
+
+		public Database()
+		{
+			var dataDir = FileSystem.AppDataDirectory;
+			var databasePath = Path.Combine(dataDir, "MauiBoardgameCafeApp.db");
+
+			string _dbEncryptionKey = SecureStorage.GetAsync("dbKey").Result;
+
+			if (string.IsNullOrEmpty(_dbEncryptionKey))
+			{
+				Guid g = new Guid();
+				_dbEncryptionKey = g.ToString();
+				SecureStorage.SetAsync("dbKey", _dbEncryptionKey);
+			}
+
+			var dbOptions = new SQLiteConnectionString(databasePath, true, key: _dbEncryptionKey);
+
+			_connection = new SQLiteAsyncConnection(dbOptions);
+
+			_ = Initialise();
+		}
+
+		private async Task Initialise()
+		{
+			await _connection.CreateTableAsync<Boardgame>();
+		}
+
+		public async Task<List<Boardgame>> GetBoardgames()
+		{
+			return await _connection.Table<Boardgame>().ToListAsync();
+		}
+
+		public async Task<Boardgame> GetBoardgame(int id)
+		{
+			var query = _connection.Table<Boardgame>().Where(t => t.Id == id);
+
+			return await query.FirstOrDefaultAsync();
+		}
+
+		public async Task<int> AddBoardgame(Boardgame item)
+		{
+			return await _connection.InsertAsync(item);
+		}
+
+		public async Task<int> DeleteBoardgame(Boardgame item)
+		{
+			return await _connection.DeleteAsync(item);
+		}
+
+		public async Task<int> UpdateBoardgame(Boardgame item)
+		{
+			return await _connection.UpdateAsync(item);
+		}
+	}
+}
---
+++ Models/Boardgame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using System.Runtime.CompilerServices;
+
+namespace MauiBoardgameCafeApp.Models
+{
+	public class Boardgame : INotifyPropertyChanged
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int PublicationYear { get; set; }
+		public int PlayersMin { get; set; }
+		public int PlayersMax { get; set; }
+		public string PlayTime { get; set; }
+		public int MinimumAge { get; set; }
+		public int CountAvailable { get; set; }
+		public int CountInUse { get; set; }
+		public int[] Ratings { get; set; }
+		public int CountRatings { get; set; }
+
+
+		#region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+		public void RaisePropertyChanged(params string[] properties)
+		{
+			foreach (var propertyName in properties)
+			{
+				PropertyChanged?.Invoke(this, new
+				PropertyChangedEventArgs(propertyName));
+			}
+		}
+		#endregion
+	}
+}
